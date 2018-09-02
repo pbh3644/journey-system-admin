cc54b21b169d5e401f954818a764722b0fe75012
pbh3644 <pbh3644@163.com>
2018-09-02 22:28:51 +0800

    Spring boot admin 监控微服务状态 后期同注册中心搭配使用

---
+++ src/main/java/com/cmrh/journey/system/admin/SystemAdminApplication.java
@@ -0,0 +1,20 @@
+package com.cmrh.journey.system.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * @author pangbohuan
+ * @description admin监控服务
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class SystemAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SystemAdminApplication.class, args);
+    }
+}
